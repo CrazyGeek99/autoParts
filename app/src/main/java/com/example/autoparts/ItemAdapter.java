908e25d7890d930507ce674d07d6a92ea92940c1
CrazyGeek99 <46688702+CrazyGeek99@users.noreply.github.com>
2019-03-17 17:05:27 +0400

    Update ItemAdapter.java

--- app/src/main/java/com/example/autoparts/ItemAdapter.java
+++ app/src/main/java/com/example/autoparts/ItemAdapter.java
@@ -1,35 +1,39 @@
 package com.example.autoparts;
 
-import android.content.ClipData;
+
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ItemAdapter extends RecyclerView.Adapter<ItemHolder> {
+public class ItemAdapter extends RecyclerView.Adapter<ItemHolder> implements Filterable {
 
-    Context iContext;
-    PartsDbHelper itemDbHelper;
-    ArrayList<Items> items;
+    private Context iContext;
+    private List<Items> itemsList = new ArrayList<>();
+    private List<Items> itemsListFilter = new ArrayList<>();
+    //private final View.OnClickListener mOnClickListener = new MyOnClickListener();
+
+    //List<Items> itemsList;
 
 
-    //how do you construct this class
-    public ItemAdapter (Context c, ArrayList<Items> items)
-    {
-        this.iContext = c;
-        this.items = items;
+
+    public ItemAdapter(List<Items> itemsList, Context context) {
+        this.itemsList = itemsList;
+        this.itemsListFilter = itemsList;
+        iContext = context;
     }
 
     //Initiating viewHolder
     @Override
     public ItemHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
 
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view,null);
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view, null);
 
         //Holder
         ItemHolder iHolder = new ItemHolder(v);
@@ -38,16 +42,60 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemHolder> {
     }
 
     @Override
-    public void onBindViewHolder(ItemHolder itemHolder, int i)
-    {
-        itemHolder.itemNameTextView.setText(items.get(i).getItem());
-        itemHolder.itemVendorTextView.setText(items.get(i).getVendor());
-        itemHolder.itemQtyTextView.setText(items.get(i).getQty());
-        itemHolder.itemPriceTextView.setText(items.get(i).getPrice());
+    public void onBindViewHolder(ItemHolder itemHolder, int i) {
+        Items item = itemsList.get(i);
+
+        itemHolder.itemNameTextView.setText(item.getItem());
+        itemHolder.itemVendorTextView.setText(item.getVendor());
+        itemHolder.itemQtyTextView.setText(Integer.toString(item.getQty()));
+        itemHolder.itemPriceTextView.setText(Integer.toString(item.getPrice()));
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        if (itemsList != null)
+            return itemsList.size();
+        else
+            return 0;
     }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                List<Items> filteredList = new ArrayList<>();
+                String searchText = constraint.toString().toLowerCase();
+                String[]split = searchText.split(",");
+                ArrayList<String> searchGenres = new ArrayList<>(split.length);
+                for (String aSplit : split){
+                    //remove spaces
+                    String trim = aSplit.trim();
+                    //skip empty entries
+                    if (trim.length() > 0)
+                        searchGenres.add(trim);
+                }
+                for (Items dataName : itemsListFilter){
+                    //filter by title
+                    if (dataName.getItem().toLowerCase().trim().contains(searchText)){
+                        filteredList.add(dataName);
+                    }
+                }
+                results.count = filteredList.size();
+                results.values = filteredList;
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                itemsList = (List<Items>)results.values;
+                notifyDataSetChanged();
+            }
+        };
+        return filter;
+    }
+
+
 }
+
